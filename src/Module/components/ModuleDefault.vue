3c96765c9d752ee755b15c1d5730ec54541c37b8
Prajit Saravanan <prajit.saravana@gmail.com>
2020-11-10 20:18:13 -0800

    💄 Worked on adding data table

--- src/Module/components/ModuleDefault.vue
+++ src/Module/components/ModuleDefault.vue
@@ -1,60 +1,24 @@
 <template>
-  <v-container class="module-default">
-    <div class="module-default__instructions">
-      <v-expansion-panels v-model="showInstructions" class="module-default__instructions" flat>
-        <v-expansion-panel>
-          <v-expansion-panel-header
-            v-show="showInstructions"
-            hide-actions
-            class="pa-0"
-            @click="showInstructions = false"
-          >
-            <template v-slot="{ open }">
-              <v-scroll-y-transition hide-on-leave>
-                <div v-if="!open" class="d-flex flex-column justify-center">
-                  <v-icon class="d-flex justify-center"> mdi-chevron-down </v-icon>
-                  <div class="text-uppercase font-weight-bold text-subtitle-2 text-center">
-                    Instructions
-                  </div>
-                </div>
-              </v-scroll-y-transition>
-            </template>
-          </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <module-instruct readonly />
-            <div @click="showInstructions = true">
-              <div class="text-uppercase font-weight-bold text-subtitle-2 text-center">close</div>
-              <!-- <div class="hr"/> OPTIONAL -->
-              <v-icon class="d-flex justify-center"> mdi-chevron-up </v-icon>
-            </div>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
-    </div>
-    <div class="module-default__container"></div>
-  </v-container>
+  <div class="module-default">
+    <v-data-table :headers="header" :items="items" sort-by="resource">
+      <template v-slot:item.finish>
+        <input type="checkbox" />
+      </template>
+      <template v-slot:item.click>
+        <v-icon> mdi-checkbox-marked-circle </v-icon>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script lang="ts">
 import { ref } from '@vue/composition-api';
-import Instruct from './ModuleInstruct.vue';
+import { items, HEADER } from './const';
 
 export default {
   name: 'ModuleDefault',
-  components: {
-    'module-instruct': Instruct
-  },
-  apollo: {},
-  data() {
-    const setupInstructions = ref({
-      description: '',
-      instructions: ['', '', '']
-    });
-    const showInstructions = ref(true);
-    return {
-      setupInstructions,
-      showInstructions
-    };
+  setup() {
+    return { header: ref(HEADER), items };
   }
 };
 </script>
---
+++ src/Module/components/const.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/prefer-default-export
+export const HEADER = [
+  { text: 'Finished', align: 'center', value: 'finish', sortable: false, width: '5%' },
+  { text: 'Resource Name', align: 'center', value: 'resource', width: '20%' },
+  { text: 'Link', value: 'link', align: 'center', sortable: false, width: '20%' },
+  { text: 'Clicked & Read', align: 'center', value: 'click', sortable: false, width: '25%' }
+];
+
+export const items = [
+  { resource: 'Use Case', link: 'test' },
+  { resource: 'Case Study', link: 'test1' },
+  { resource: 'Article', link: 'test2' },
+  { resource: 'Press Release', link: 'test3' },
+  { resource: 'Documentation', link: 'test4' }
+];
