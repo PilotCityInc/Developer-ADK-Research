ce4c1a42c471da78755bdb0f0c5c2b21175f5a83
Derick Lee <dericklee@pilotcity.com>
2021-02-01 09:11:31 +0000

    changes

--- src/Module/components/ModuleInstruct.vue
+++ src/Module/components/ModuleInstruct.vue
@@ -1,55 +1,57 @@
 <template>
-  <ValidationObserver v-slot="{ invalid }" slim>
-    <!--  TODO: make the inputs into actual components -->
-    <v-container class="module-instruct">
-      <div class="module-instruct__container">
-        <div class="module-instruct__description">
-          <div class="module-instruct__description-label">
-            <span>Goal</span>
-          </div>
-
-          <v-textarea
-            v-model="goal"
-            row-height="3"
-            rows="3"
-            outlined
-            class="font-weight-bold text-body-1"
-            hide-details
-            dense
-          ></v-textarea>
+  <!-- <ValidationObserver v-slot="{ invalid }" slim> -->
+  <!--  TODO: make the inputs into actual components -->
+  <v-container class="module-instruct">
+    <div class="module-instruct__container">
+      <div class="module-instruct__description">
+        <div class="module-instruct__description-label">
+          <span>Goal</span>
         </div>
-        <div class="module-instruct__instructions">
-          <div class="module-instruct__description-label">
-            <span>Instructions</span>
-          </div>
-          <div
-            v-for="(i, index) in researchInstructions"
-            :key="index"
-            class="module-instruct__instructions-item"
+
+        <v-textarea
+          v-model="goal"
+          row-height="3"
+          rows="3"
+          outlined
+          class="font-weight-bold text-body-1"
+          hide-details
+          dense
+          disabled
+        ></v-textarea>
+      </div>
+      <div class="module-instruct__instructions">
+        <div class="module-instruct__description-label">
+          <span>Instructions</span>
+        </div>
+        <div
+          v-for="(i, index) in researchInstructions"
+          :key="index"
+          class="module-instruct__instructions-item"
+        >
+          <v-avatar
+            size="35"
+            color="white"
+            class="module-instruct__instructions-av font-weight-bold text-caption d-none d-sm-flex"
           >
-            <v-avatar
-              size="35"
-              color="white"
-              class="module-instruct__instructions-av font-weight-bold text-caption d-none d-sm-flex"
-            >
-              {{ index + 1 }}
-            </v-avatar>
+            {{ index + 1 }}
+          </v-avatar>
 
-            <validation-provider v-slot="{ errors }" slim rules="required">
-              <v-textarea
-                v-model="researchInstructions[index]"
-                row-height="3"
-                rows="1"
-                outlined
-                hide-details
-                dense
-                :error-messages="errors"
-                class="font-weight-bold text-body-1"
-              ></v-textarea>
-            </validation-provider>
-          </div>
+          <validation-provider v-slot="{ errors }" slim rules="required">
+            <v-textarea
+              v-model="researchInstructions[index]"
+              disabled
+              row-height="3"
+              rows="1"
+              outlined
+              hide-details
+              dense
+              :error-messages="errors"
+              class="font-weight-bold text-body-1"
+            ></v-textarea>
+          </validation-provider>
+        </div>
 
-          <div>
+        <!-- <div>
             <v-btn
               class="module-instruct__instructions-add font-weight-black text-body-1"
               depressed
@@ -60,11 +62,11 @@
             >
               <v-icon class="module-instruct__instructions-add-icon"> mdi-plus </v-icon>
             </v-btn>
-          </div>
-        </div>
+          </div> -->
       </div>
-    </v-container>
-  </ValidationObserver>
+    </div>
+  </v-container>
+  <!-- </ValidationObserver> -->
 </template>
 
 <script lang="ts">
