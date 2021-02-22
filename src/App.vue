1c6a7bba9a5e4e04e6edfd07946ce0613b474851
juxsta <ericjrreyes2@gmail.com>
2021-02-22 11:13:00 -0800

    save to update

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default defineComponent({
       data: {
         adks: []
       },
-      save: () => {
+      update: () => {
         return new Promise((resolve, reject) => {
           setTimeout(() => {
             resolve(true);
